f8e6cfa1367e2dba78c545c098d4a91403e357a2
John Privitera <johnpriv@live.com>
2021-10-12 19:26:13 -0400

    Slight updates to tree-tool. Commit is to prevent possible conflicts 2021/10/12

--- source/tree_tool.py
+++ source/tree_tool.py
@@ -2,12 +2,26 @@ from backend import load_spreadsheet
 
 
 class tree_node:
-    def __init__(self, id_val, parent=None, children=None):
+    def __init__(self, id_val, node_type=None, prompts=None):
         self.id_val = id_val
-        self.parent = parent
-        self.children = children
+        self.node_type = node_type
+        self.children = None
+        self.prompts = prompts
 
-    # def check_for_cycle(self):
+    def get_prompts(self):
+        return self.prompts
+
+    def get_node_type(self):
+        return self.node_type
+
+    def add_child(self, child_id, child_type, child_prompts):
+        print('temp')
+
+    # def create_children(self):
+    #     for prompt in self.prompts:
+    #         if self.node_type is 'question':
+    #             self.children.append(tree_node(id_val=prompt, node_type='response', prompts=None))
+    #     # def check_for_cycle(self):
 
 
 def check_tree():
