31ddfe343387663545f28943c0da99421b7408ca
John Privitera <johnpriv@live.com>
2021-11-18 17:48:14 -0500

    Added new prompt types; will update later

--- backend/prompts.py
+++ backend/prompts.py
@@ -20,3 +20,27 @@ class response_prompt:
 
     def get_question_id(self):
         return self.question_id
+
+
+class solutions_prompt:
+    def __init__(self, text, info_listing_ids):
+        self.text = text
+        self.info_listing_ids = list(filter(None, info_listing_ids))
+
+    def get_text(self):
+        return self.text
+
+    def get_info_listing_ids(self):
+        return self.info_listing_ids
+
+
+class info_listing_prompt:
+    def __init__(self, text, info_link):
+        self.text = text
+        self.info_link = info_link
+
+    def get_text(self):
+        return self.text
+
+    def get_info_link(self):
+        return self.info_link
