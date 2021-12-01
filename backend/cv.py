92a2c8803440db6c0a7eaea5c74bd253d5d1c15e
Mark-Morton <mmortonred@gmail.com>
2021-12-01 17:45:13 -0500

    Improved CV algorithm

--- backend/cv.py
+++ backend/cv.py
@@ -4,7 +4,11 @@ import pprint
 from picamera import PiCamera
 from PIL import Image
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
+def convertToRGB(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 def init_cam():
     #sets up Picam, can be altered for normal webcam
     camera = PiCamera()
@@ -15,15 +19,22 @@ def init_cam():
 def get_emotion(camera):
     
     result=[]
+    faces_rects=[]
     ##repeatedly takes photos and checks each for valid face with expression, can change later for more specificity in selection
-    while not result:
+    while not np.any(faces_rects):
         camera.capture("/home/pi/fer-21.0.5/tests/sample.jpg")
         image = cv2.imread("/home/pi/fer-21.0.5/tests/sample.jpg")
-        detector = FER()
-        result = detector.detect_emotions(image)
-        result2 = detector.top_emotion(image)
-        print('iterated')
+        test_image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        haar_cascade_face = cv2.CascadeClassifier('/home/pi/opencv/data/haarcascades/haarcascade_frontalface_alt2.xml')
+        faces_rects = haar_cascade_face.detectMultiScale(test_image_gray, scaleFactor = 1.2, minNeighbors = 5);
+        print('Faces found: ', len(faces_rects))
         #Remainder of code only necessary for visual display of info, if the program only needs to know emotions internally, program can stop here
+    for (x,y,w,h) in faces_rects:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    plt.imshow(convertToRGB(image))
+    detector = FER()
+    result = detector.detect_emotions(image)
+    result2 = detector.top_emotion(image)
     bounding_box = result[0]["box"]
     emotions = result[0]["emotions"]
     
@@ -65,3 +76,4 @@ if __name__ == '__main__':
     for i in range(3):
         print(get_emotion(camera))
     camera.close()
+
