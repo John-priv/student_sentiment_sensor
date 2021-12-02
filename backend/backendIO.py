b1b0a233343b4520aaea969a8f2f4e96e1ffb56c
John Privitera <johnpriv@live.com>
2021-12-02 00:21:33 -0500

    Change logging: 'Emotion' to 'Emotion_CV'

--- backend/backendIO.py
+++ backend/backendIO.py
@@ -62,7 +62,7 @@ def store_interaction(convo, emotion, u_id):
         for element in line:
             write_text = write_text + (str(element)) + ','  # separate elements by comma
         write_text = write_text[:-1] + '\n'                 # trim off trailing comma, add newline
-    write_text = write_text + 'Emotion,' + emotion                       # end file with emotion
+    write_text = write_text + 'Emotion_CV,' + emotion       # end file with emotion
     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../data/stored_user_data/" + u_id + ".dat"))
     write_file = open(file_path, "w")
     write_file.write(write_text)
